bbb30fd265aac2b2622798b61159858e419dcf11
Djalma Henrique <djalmahenry@outlook.com>
2023-07-09 12:02:05 -0300

    feat: refactoring API

---
+++ db/conn.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+async function main() {
+  try {
+    mongoose.set("strictQuery", true);
+
+    await mongoose.connect(
+      "mongodb+srv://djalmahenry:test1103@cluster0.emhndpf.mongodb.net/?retryWrites=true&w=majority"
+    );
+
+    console.log("Conectado!");
+  } catch (error) {
+    console.log(`Erro: ${error}`);
+  }
+}
+
+module.exports = main;
